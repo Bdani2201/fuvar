139f7532c87760c94dd48ebf3ae76c3afe920873
Bdani2201 <bdani2201@gmail.com>
2022-01-21 10:17:50 +0100

    Add project files.

---
+++ fuvar/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fuvar
+{
+    class Program
+    {
+        struct fuvarozas
+        {
+            public int taxiid;
+            public string indulas;
+            public int idotartam;
+            public double tavolsag;
+            public double viteldij;
+            public double borravalo;
+            public string fizetesmod;
+        }
+        static fuvarozas[] adatok = new fuvarozas[2000];
+        struct fuvarozas2
+        {
+            public int taxiid;
+            public string indulas;
+            public int idotartam;
+            public double tavolsag;
+            public double viteldij;
+            public double borravalo;
+            public string fizetesmod;
+        }
+        static fuvarozas2[] adatok2 = new fuvarozas2[2000];
+        static void Main(string[] args)
+        {
+            string[] fajlbol = File.ReadAllLines("fuvar.csv");
+            int sorokszama = 0;
+            int i, j;
+            for (int k = 1; k < fajlbol.Count(); k++)
+            {
+                string[] egysordarabolva = fajlbol[k].Split(';');
+                adatok[sorokszama].taxiid = Convert.ToInt32(egysordarabolva[0]);
+                adatok[sorokszama].indulas = egysordarabolva[1];
+                adatok[sorokszama].idotartam = Convert.ToInt32(egysordarabolva[2]);
+                adatok[sorokszama].tavolsag = Convert.ToDouble(egysordarabolva[3]);
+                adatok[sorokszama].viteldij = Convert.ToDouble(egysordarabolva[4]);
+                adatok[sorokszama].borravalo = Convert.ToDouble(egysordarabolva[5]);
+                adatok[sorokszama].fizetesmod = egysordarabolva[6];
+                sorokszama++;
+            }
+            int fuvarokszama = sorokszama;
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
